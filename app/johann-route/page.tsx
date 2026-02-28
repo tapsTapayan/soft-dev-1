5ce7b45de7070707533da6e29603ca1f318f8f36
Hansssel <bolabonjohann1@gmail.com>
2026-02-28 05:20:14 +0000

    [Feature][BOLABON]i created my own route

---
+++ app/johann-route/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './johann.module.css'
+
+function Johannpage() {
+  return (
+    <div className={styles.nameContainer}>Johann O. Bolabon</div>
+  )
+}
+
+export default Johannpage
